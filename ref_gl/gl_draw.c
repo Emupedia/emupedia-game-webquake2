ebf708868a8b1e312849b22ba6083b6fcbd096af
Turo Lamminen <turotl@gmail.com>
2014-07-11 12:39:45 +0300

    Whitespace

--- ref_gl/gl_draw.c
+++ ref_gl/gl_draw.c
@@ -435,14 +435,17 @@ void EXPORT Draw_Fill (int x, int y, int w, int h, int c)
 		color.v[1]/255.0f,
 		color.v[2]/255.0f);
 
-	qglBegin (GL_QUADS);
+	qglBegin(GL_QUADS);
 
-	qglVertex2f(x,y);
-	qglVertex2f(x+w, y);
-	qglVertex2f(x+w, y+h);
-	qglVertex2f(x, y+h);
+	qglVertex2f(x, y);
 
-	qglEnd ();
+	qglVertex2f(x + w, y);
+
+	qglVertex2f(x + w, y + h);
+
+	qglVertex2f(x, y + h);
+
+	qglEnd();
 
 	qglColor3f (1,1,1);
 
