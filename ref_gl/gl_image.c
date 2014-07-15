2d2ec55a225ff5961fee94ae05ee9550f78f3223
Turo Lamminen <turotl@gmail.com>
2014-07-15 12:44:54 +0300

    Fix left shift undefined behavior

--- ref_gl/gl_image.c
+++ ref_gl/gl_image.c
@@ -3318,7 +3318,7 @@ int Draw_GetPalette (void)
 		g = pal[i*3+1];
 		b = pal[i*3+2];
 		
-		v = (255<<24) + (r<<0) + (g<<8) + (b<<16);
+		v = (255UL<<24) + (r<<0) + (g<<8) + (b<<16);
 		d_8to24table[i] = LittleLong(v);
 
 		d_8to24float[i][0] = r / 255.0f;
