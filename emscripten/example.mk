34539c6d4fff56741a90d45e5203a90744588f89
Turo Lamminen <turotl@gmail.com>
2014-07-17 13:45:17 +0300

    Adjust emscripten default options

--- emscripten/example.mk
+++ emscripten/example.mk
@@ -6,7 +6,7 @@
 TOPDIR:=..
 
 
-LTO:=n
+LTO:=y
 ASAN:=n
 TSAN:=n
 UBSAN:=n
@@ -18,10 +18,13 @@ CC:=emcc
 CXX:=em++
 CFLAGS:=-g -DNDEBUG -DLINUX -std=c99 -D_GNU_SOURCE=1
 CFLAGS+=-DREF_HARD_LINKED
-OPTFLAGS:=-fno-strict-aliasing
+OPTFLAGS:=-O3
 
 
-LDFLAGS:=-g -s ALLOW_MEMORY_GROWTH=1 --preload-file baseq2
+LDFLAGS:=-g --preload-file baseq2
+LDFLAGS+=-s ALLOW_MEMORY_GROWTH=1
+LDFLAGS+=-s OUTLINING_LIMIT=5000
+LDFLAGS+=-s FORCE_ALIGNED_MEMORY=1
 LDLIBS:=
 LDLIBS_ref_gl:=
 LDLIBS_client:=
