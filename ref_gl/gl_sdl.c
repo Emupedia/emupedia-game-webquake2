95d1d8ca00f17ba656ab120497a840fb8a7e3607
Turo Lamminen <turotl@gmail.com>
2014-07-22 12:38:49 +0300

    Fix glDebugCallback type to match epoxy GLDEBUGPROC

--- ref_gl/gl_sdl.c
+++ ref_gl/gl_sdl.c
@@ -926,7 +926,7 @@ static const char *errorType(GLenum type)
 #endif
 
 
-void CALLBACK glDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const char *message, void *userParam)
+void CALLBACK glDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam)
 {
 	switch (severity)
 	{
