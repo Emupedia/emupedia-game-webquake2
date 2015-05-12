40712ba09cde848fc6b5b219273f870d453edb6b
Tuomas Närväinen <tuomas.narvainen@alternativegames.net>
2015-05-12 15:39:37 +0300

    Add an include.

--- game/q_shared.h
+++ game/q_shared.h
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include <time.h>
 #include <ctype.h>
