92508f90990b1821ef84f94dd8141263d6c63e3d
Turo Lamminen <turotl@gmail.com>
2015-06-13 00:38:31 +0300

    Move contents of qgl.h to gl_local.h

--- ref_gl/gl_local.h
+++ ref_gl/gl_local.h
@@ -35,7 +35,96 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "../client/ref.h"
 
-#include "qgl.h"
+
+#ifdef _WIN32
+#  include <windows.h>
+#endif
+
+
+#ifdef USE_GLEW
+
+#include <GL/glew.h>
+
+#else  // USE_GLEW
+
+
+#define GL_GLEXT_PROTOTYPES 1
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+
+#endif  // USE_GLEW
+
+
+qboolean QGL_Init( const char *dllname );
+void     QGL_Shutdown( void );
+
+#ifndef APIENTRY
+#  define APIENTRY
+#endif
+
+//extern  void ( APIENTRY * qglAccum )(GLenum op, GLfloat value);
+void qglAlphaFunc(GLenum func, GLclampf ref);
+void qglBegin(GLenum mode);
+void qglBindTexture(GLenum target, GLuint texture);
+extern  void ( APIENTRY * qglBlendFunc )(GLenum sfactor, GLenum dfactor);
+extern  void ( APIENTRY * qglClear )(GLbitfield mask);
+extern  void ( APIENTRY * qglClearColor )(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
+extern  void ( APIENTRY * qglClearDepth )(GLclampd depth);
+extern  void ( APIENTRY * qglClearStencil )(GLint s);
+void qglColor3f(GLfloat red, GLfloat green, GLfloat blue);
+void qglColor4f(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
+extern  void ( APIENTRY * qglColorMask )(GLboolean red, GLboolean green, GLboolean blue, GLboolean alpha);
+extern  void ( APIENTRY * qglCullFace )(GLenum mode);
+extern  void ( APIENTRY * qglDeleteTextures )(GLsizei n, const GLuint *textures);
+extern  void ( APIENTRY * qglDepthFunc )(GLenum func);
+extern  void ( APIENTRY * qglDepthMask )(GLboolean flag);
+void qglDepthRange(GLclampd zNear, GLclampd zFar);
+void qglDisable(GLenum cap);
+extern  void ( APIENTRY * qglDrawArrays )(GLenum mode, GLint first, GLsizei count);
+extern  void ( APIENTRY * qglDrawElements )(GLenum mode, GLsizei count, GLenum type, const GLvoid *indices);
+void qglEnable(GLenum cap);
+void qglEnd(void);
+extern  void ( APIENTRY * qglFrontFace )(GLenum mode);
+void qglFrustum(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble zNear, GLdouble zFar);
+extern  void ( APIENTRY * qglGenTextures )(GLsizei n, GLuint *textures);
+extern  GLenum ( APIENTRY * qglGetError )(void);
+void qglGetFloatv(GLenum pname, GLfloat *params);
+extern  const GLubyte * ( APIENTRY * qglGetString )(GLenum name);
+extern  void ( APIENTRY * qglHint )(GLenum target, GLenum mode);
+extern  GLboolean ( APIENTRY * qglIsEnabled )(GLenum cap);
+extern  GLboolean ( APIENTRY * qglIsTexture )(GLuint texture);
+void qglLoadIdentity(void);
+void qglLoadMatrixf(const GLfloat *m);
+void qglMatrixMode(GLenum mode);
+void qglMultMatrixf(const GLfloat *m);
+void qglMTexCoord2f(GLenum tex, GLfloat s, GLfloat t);
+void qglOrtho(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble zNear, GLdouble zFar);
+extern  void ( APIENTRY * qglPolygonOffset )(GLfloat factor, GLfloat units);
+void qglPopMatrix(void);
+void qglPushMatrix(void);
+extern  void ( APIENTRY * qglReadPixels )(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *pixels);
+void qglRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z);
+void qglScalef(GLfloat x, GLfloat y, GLfloat z);
+extern  void ( APIENTRY * qglScissor )(GLint x, GLint y, GLsizei width, GLsizei height);
+extern  void ( APIENTRY * qglStencilFunc )(GLenum func, GLint ref, GLuint mask);
+extern  void ( APIENTRY * qglStencilMask )(GLuint mask);
+extern  void ( APIENTRY * qglStencilOp )(GLenum fail, GLenum zfail, GLenum zpass);
+void qglTexEnvi(GLenum target, GLenum pname, GLint param);
+extern  void ( APIENTRY * qglTexImage2D )(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid *pixels);
+extern  void ( APIENTRY * qglTexParameterf )(GLenum target, GLenum pname, GLfloat param);
+extern  void ( APIENTRY * qglTexParameterfv )(GLenum target, GLenum pname, const GLfloat *params);
+extern  void ( APIENTRY * qglTexParameteri )(GLenum target, GLenum pname, GLint param);
+extern  void ( APIENTRY * qglTexParameteriv )(GLenum target, GLenum pname, const GLint *params);
+extern  void ( APIENTRY * qglTexSubImage2D )(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height, GLenum format, GLenum type, const GLvoid *pixels);
+void qglTranslatef(GLfloat x, GLfloat y, GLfloat z);
+void qglVertex2f(GLfloat x, GLfloat y);
+void qglVertex3f(GLfloat x, GLfloat y, GLfloat z);
+extern  void ( APIENTRY * qglViewport )(GLint x, GLint y, GLsizei width, GLsizei height);
+
+void qglActiveTexture(GLenum);
+
 
 //please keep this undefined on modified versions.
 #define R1GL_RELEASE 1
