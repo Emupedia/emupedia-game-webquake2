013383554e1a05f805fbedda5d020124740b08c8
Turo Lamminen <turotl@gmail.com>
2015-09-30 13:38:15 +0300

    Fix some format string warnings

--- ctf/g_cmds.c
+++ ctf/g_cmds.c
@@ -305,7 +305,7 @@ void Cmd_God_f(edict_t *ent){
 	else
 		msg = "godmode ON\n";
 		
-	gi.cprintf(ent, PRINT_HIGH, msg);
+	gi.cprintf(ent, PRINT_HIGH, "%s", msg);
 }
 
 
@@ -332,7 +332,7 @@ void Cmd_Notarget_f(edict_t *ent){
 	else
 		msg = "notarget ON\n";
 		
-	gi.cprintf(ent, PRINT_HIGH, msg);
+	gi.cprintf(ent, PRINT_HIGH, "%s", msg);
 }
 
 
@@ -359,7 +359,7 @@ void Cmd_Noclip_f(edict_t *ent){
 		msg = "noclip ON\n";
 	}
 	
-	gi.cprintf(ent, PRINT_HIGH, msg);
+	gi.cprintf(ent, PRINT_HIGH, "%s", msg);
 }
 
 
