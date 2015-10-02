786026e425bcf7126c44caffc00fd4aafd01ac4a
Turo Lamminen <turotl@gmail.com>
2015-10-02 13:23:11 +0300

    Alphabetize

--- qcommon/net_websockets.cpp
+++ qcommon/net_websockets.cpp
@@ -49,6 +49,7 @@ int			server_port = 0;
 static int			ip_sockets[2] = { 0, 0 };
 
 char *NET_ErrorString (void);
+qboolean	NET_StringToSockaddr (const char *s, struct sockaddr *sadr);
 
 cvar_t	*net_no_recverr = NULL;
 
@@ -90,6 +91,47 @@ void Net_Stats_f (void);
 netadr_t	net_proxy_addr;
 qboolean	net_proxy_active;
 
+void NetadrToSockadr (netadr_t *a, struct sockaddr_in *s)
+{
+	memset (s, 0, sizeof(*s));
+
+	if (a->type == NA_IP)
+	{
+		s->sin_family = AF_INET;
+
+		*(int *)&s->sin_addr = *(int *)&a->ip;
+		s->sin_port = a->port;
+	}
+	else if (a->type == NA_BROADCAST)
+	{
+		s->sin_family = AF_INET;
+
+		s->sin_port = a->port;
+		*(int *)&s->sin_addr = -1;
+	}
+}
+
+
+char	*NET_AdrToString (netadr_t *a)
+{
+	static	char	s[32];
+	
+	Com_sprintf (s, sizeof(s), "%i.%i.%i.%i:%i", a->ip[0], a->ip[1], a->ip[2], a->ip[3], ntohs(a->port));
+
+	return s;
+}
+
+
+char	*NET_BaseAdrToString (netadr_t *a)
+{
+	static	char	s[32];
+	
+	Com_sprintf (s, sizeof(s), "%i.%i.%i.%i", a->ip[0], a->ip[1], a->ip[2], a->ip[3]);
+
+	return s;
+}
+
+
 void NET_Common_Init (void)
 {
 	net_ignore_icmp = Cvar_Get ("net_ignore_icmp", "0", 0);
@@ -98,10 +140,66 @@ void NET_Common_Init (void)
 	Cmd_AddCommand ("net_stats", Net_Stats_f);
 }
 
+
+uint32 NET_htonl (uint32 ip)
+{
+	return htonl (ip);
+}
+
+
+char	*NET_inet_ntoa (uint32 ip)
+{
+	return inet_ntoa (*(struct in_addr *)&ip);
+}
+
+
+uint32 NET_ntohl (uint32 ip)
+{
+	return ntohl (ip);
+}
+
+
 #define SockadrToNetadr(s,a) \
 	a->type = NA_IP; \
 	*(int *)&a->ip = ((struct sockaddr_in *)s)->sin_addr.s_addr; \
 	a->port = ((struct sockaddr_in *)s)->sin_port; \
+
+
+/*
+=============
+NET_StringToAdr
+
+localhost
+idnewt
+idnewt:28000
+192.246.40.70
+192.246.40.70:28000
+=============
+*/
+qboolean	NET_StringToAdr (const char *s, netadr_t *a)
+{
+	struct sockaddr sadr;
+
+	if (!strcmp (s, "localhost"))
+	{
+		memset (a, 0, sizeof(*a));
+
+		//r1: should need some kind of ip data to prevent comparisons with empty ips?
+		a->ip[0] = 127;
+		a->ip[3] = 1;
+		a->port = PORT_SERVER;
+		a->type = NA_LOOPBACK;
+		return true;
+	}
+
+	if (!NET_StringToSockaddr (s, &sadr))
+		return false;
+	
+	SockadrToNetadr (&sadr, a);
+
+	return true;
+}
+
 
 qboolean	NET_StringToSockaddr (const char *s, struct sockaddr *sadr)
 {
@@ -169,95 +267,6 @@ qboolean	NET_StringToSockaddr (const char *s, struct sockaddr *sadr)
 	return true;
 }
 
-/*
-=============
-NET_StringToAdr
-
-localhost
-idnewt
-idnewt:28000
-192.246.40.70
-192.246.40.70:28000
-=============
-*/
-qboolean	NET_StringToAdr (const char *s, netadr_t *a)
-{
-	struct sockaddr sadr;
-
-	if (!strcmp (s, "localhost"))
-	{
-		memset (a, 0, sizeof(*a));
-
-		//r1: should need some kind of ip data to prevent comparisons with empty ips?
-		a->ip[0] = 127;
-		a->ip[3] = 1;
-		a->port = PORT_SERVER;
-		a->type = NA_LOOPBACK;
-		return true;
-	}
-
-	if (!NET_StringToSockaddr (s, &sadr))
-		return false;
-	
-	SockadrToNetadr (&sadr, a);
-
-	return true;
-}
-
-
-void NetadrToSockadr (netadr_t *a, struct sockaddr_in *s)
-{
-	memset (s, 0, sizeof(*s));
-
-	if (a->type == NA_IP)
-	{
-		s->sin_family = AF_INET;
-
-		*(int *)&s->sin_addr = *(int *)&a->ip;
-		s->sin_port = a->port;
-	}
-	else if (a->type == NA_BROADCAST)
-	{
-		s->sin_family = AF_INET;
-
-		s->sin_port = a->port;
-		*(int *)&s->sin_addr = -1;
-	}
-}
-
-char	*NET_inet_ntoa (uint32 ip)
-{
-	return inet_ntoa (*(struct in_addr *)&ip);
-}
-
-uint32 NET_htonl (uint32 ip)
-{
-	return htonl (ip);
-}
-
-uint32 NET_ntohl (uint32 ip)
-{
-	return ntohl (ip);
-}
-
-char	*NET_AdrToString (netadr_t *a)
-{
-	static	char	s[32];
-	
-	Com_sprintf (s, sizeof(s), "%i.%i.%i.%i:%i", a->ip[0], a->ip[1], a->ip[2], a->ip[3], ntohs(a->port));
-
-	return s;
-}
-
-char	*NET_BaseAdrToString (netadr_t *a)
-{
-	static	char	s[32];
-	
-	Com_sprintf (s, sizeof(s), "%i.%i.%i.%i", a->ip[0], a->ip[1], a->ip[2], a->ip[3]);
-
-	return s;
-}
-
 
 /*
 ====================
@@ -310,6 +319,73 @@ void NET_SetProxy (netadr_t *proxy)
 	else
 		net_proxy_active = false;
 }
+
+
+#define	MAX_LOOPBACK	4
+
+typedef struct
+{
+	byte	data[MAX_MSGLEN];
+	int		datalen;
+} loopmsg_t;
+
+typedef struct
+{
+	loopmsg_t	msgs[MAX_LOOPBACK];
+	int			get, send;
+} loopback_t;
+
+loopback_t	loopbacks[2];
+
+
+int NET_Client_Sleep (int msec)
+{
+    struct timeval	timeout;
+	fd_set			fdset;
+	SOCKET			i;
+
+	FD_ZERO(&fdset);
+	i = 0;
+
+	if (ip_sockets[NS_CLIENT])
+	{
+		FD_SET(ip_sockets[NS_CLIENT], &fdset); // network socket
+		i = ip_sockets[NS_CLIENT];
+	}
+
+	timeout.tv_sec = msec/1000;
+	timeout.tv_usec = (msec%1000)*1000;
+	return select ((int)(i+1), &fdset, NULL, NULL, &timeout);
+}
+
+
+qboolean	NET_GetLoopPacket (netsrc_t sock, netadr_t *net_from, sizebuf_t *net_message)
+{
+	int		i;
+	loopback_t	*loop;
+
+	loop = &loopbacks[sock];
+
+	if (loop->send - loop->get > MAX_LOOPBACK)
+		loop->get = loop->send - MAX_LOOPBACK;
+
+	if (loop->get >= loop->send)
+		return false;
+
+	i = loop->get & (MAX_LOOPBACK-1);
+	loop->get++;
+
+	memcpy (net_message->data, loop->msgs[i].data, loop->msgs[i].datalen);
+	net_message->cursize = loop->msgs[i].datalen;
+	memset (net_from, 0, sizeof(*net_from));
+	net_from->type = NA_LOOPBACK;
+	net_from->ip[0] = 127;
+	net_from->ip[3] = 1;
+	net_from->port = PORT_SERVER;
+	return true;
+
+}
+
 
 /*
 ====================
@@ -376,6 +452,30 @@ void NET_OpenIP (int flags)
 		Com_Error (ERR_DROP, "Couldn't allocate client IP port.");
 }
 
+
+void Net_Restart_f (void)
+{
+	int old;
+	old = NET_Config (NET_NONE);
+	NET_Config (old);
+}
+
+
+void NET_SendLoopPacket (netsrc_t sock, int length, const void *data)
+{
+	int		i;
+	loopback_t	*loop;
+
+	loop = &loopbacks[sock^1];
+
+	i = loop->send & (MAX_LOOPBACK-1);
+	loop->send++;
+
+	memcpy (loop->msgs[i].data, data, length);
+	loop->msgs[i].datalen = length;
+}
+
+
 // sleeps msec or until net socket is ready
 #ifndef NO_SERVER
 void NET_Sleep(int msec)
@@ -398,95 +498,8 @@ void NET_Sleep(int msec)
 }
 #endif
 
-void Net_Restart_f (void)
-{
-	int old;
-	old = NET_Config (NET_NONE);
-	NET_Config (old);
-}
 
-
-#define	MAX_LOOPBACK	4
-
-typedef struct
-{
-	byte	data[MAX_MSGLEN];
-	int		datalen;
-} loopmsg_t;
-
-typedef struct
-{
-	loopmsg_t	msgs[MAX_LOOPBACK];
-	int			get, send;
-} loopback_t;
-
-loopback_t	loopbacks[2];
-
-qboolean	NET_GetLoopPacket (netsrc_t sock, netadr_t *net_from, sizebuf_t *net_message)
-{
-	int		i;
-	loopback_t	*loop;
-
-	loop = &loopbacks[sock];
-
-	if (loop->send - loop->get > MAX_LOOPBACK)
-		loop->get = loop->send - MAX_LOOPBACK;
-
-	if (loop->get >= loop->send)
-		return false;
-
-	i = loop->get & (MAX_LOOPBACK-1);
-	loop->get++;
-
-	memcpy (net_message->data, loop->msgs[i].data, loop->msgs[i].datalen);
-	net_message->cursize = loop->msgs[i].datalen;
-	memset (net_from, 0, sizeof(*net_from));
-	net_from->type = NA_LOOPBACK;
-	net_from->ip[0] = 127;
-	net_from->ip[3] = 1;
-	net_from->port = PORT_SERVER;
-	return true;
-
-}
-
-
-void NET_SendLoopPacket (netsrc_t sock, int length, const void *data)
-{
-	int		i;
-	loopback_t	*loop;
-
-	loop = &loopbacks[sock^1];
-
-	i = loop->send & (MAX_LOOPBACK-1);
-	loop->send++;
-
-	memcpy (loop->msgs[i].data, data, length);
-	loop->msgs[i].datalen = length;
-}
-
-
-int NET_Client_Sleep (int msec)
-{
-    struct timeval	timeout;
-	fd_set			fdset;
-	SOCKET			i;
-
-	FD_ZERO(&fdset);
-	i = 0;
-
-	if (ip_sockets[NS_CLIENT])
-	{
-		FD_SET(ip_sockets[NS_CLIENT], &fdset); // network socket
-		i = ip_sockets[NS_CLIENT];
-	}
-
-	timeout.tv_sec = msec/1000;
-	timeout.tv_usec = (msec%1000)*1000;
-	return select ((int)(i+1), &fdset, NULL, NULL, &timeout);
-}
-
-
-}
+}  // extern "C"
 
 
 #ifdef NDEBUG
