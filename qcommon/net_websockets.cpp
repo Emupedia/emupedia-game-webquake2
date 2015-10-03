f11287137848e48facf9e7a7184a0f464c81aa8b
Turo Lamminen <turotl@gmail.com>
2015-10-03 22:33:44 +0300

    Silence a warning

--- qcommon/net_websockets.cpp
+++ qcommon/net_websockets.cpp
@@ -200,8 +200,8 @@ static int websocketCallback(struct libwebsocket_context *context, struct libweb
 
 // not const because libwebsockets writes to it
 static struct libwebsocket_protocols protocols[] = {
-	  { "quake2", websocketCallback, 0, 0 } /* end */
-	, { NULL, NULL, 0, 0 } /* end */
+	  { "quake2", websocketCallback, 0, 0, 0, NULL, NULL, 0 }
+	, { NULL, NULL, 0, 0, 0, NULL, NULL, 0  } /* end */
 };
 
 
