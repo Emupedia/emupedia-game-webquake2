975fb73bb9ba3deec9ca01411cf76ddf1c8296ce
Turo Lamminen <turotl@gmail.com>
2016-10-20 13:18:57 +0300

    Remove dead code

--- ctfgame/g_combat.c
+++ ctfgame/g_combat.c
@@ -125,11 +125,8 @@ SpawnDamage
 ================
 */
 void SpawnDamage(int type, vec3_t origin, vec3_t normal, int damage){
-	if(damage > 255)
-		damage = 255;
 	gi.WriteByte(svc_temp_entity);
 	gi.WriteByte(type);
-	//	gi.WriteByte(damage);
 	gi.WritePosition(origin);
 	gi.WriteDir(normal);
 	gi.multicast(origin, MULTICAST_PVS);
